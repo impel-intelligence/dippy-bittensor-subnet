312c94fd4c7c284a033fe7ce14523d481f3f52ff
Manav Shah <manav@tryimpel.com>
2024-04-15 20:17:58 +0000

    evaluation score on dataset

---
+++ dippy/dataset.py
@@ -0,0 +1,114 @@
+import json
+import random
+import jinja2
+
+from transformers import AutoTokenizer
+from torch.utils.data import Dataset
+
+
+class PippaDataset(Dataset):
+    def __init__(self, filename):
+        self.filename = filename
+        with open(filename, 'r') as f:
+            data = [json.loads(line) for line in f]
+
+        self.dataset = self.process_data(data)
+
+        self._chat_template = None
+        self._tokenizer = None
+
+    def set_chat_template_params(self, template_path: str, tokenizer: AutoTokenizer):
+        self._chat_template = jinja2.Template(open(template_path).read())
+        self._tokenizer = tokenizer
+
+    def process_data(data):
+        """
+        Convert pippa dataset to a format that can be used downstream.
+        """
+        converted_dataset = []
+        for data_point in data:
+            # construct the system prompt using the bot_description and bot_greeting
+            if not data_point['bot_definitions']:
+                data_point['bot_definitions'] = ''
+
+            if not data_point['categories']:
+                data_point['categories'] = 'None'
+
+            system_prompt = f"""A chat between a user and a curious artificial intelligence that is expert at roleplay. 
+The AI is roleplaying as a character named {data_point['bot_name']}. 
+The character's description: {data_point['bot_description']}. {data_point['bot_definitions']}.
+The themes of the conversation are: {data_point['categories']}."""
+
+            messages = [{
+                'role': 'system',
+                'content': system_prompt
+            }]
+
+            messages.append(
+                {
+                    'role': 'assistant',
+                    'content': f"{data_point['bot_name']}: {data_point['bot_greeting']}"
+                }
+            )
+
+            # get index of the last message from the chatbot
+            last_message_index = 0
+            for i, message in enumerate(data_point['conversation']):
+                if not message['is_human']:
+                    last_message_index = i
+            
+            for message in data_point['conversation'][:last_message_index]:
+                if message['is_human']:
+                    messages.append(
+                        {
+                            'role': 'user',
+                            'content': message['message']
+                        }
+                    )
+                else:
+                    messages.append(
+                        {
+                            'role': 'assistant',
+                            'content': f"{message['message']}"
+                        }
+                    )
+            
+            character_response = data_point['conversation'][last_message_index]['message']
+
+            converted_dataset.append(
+                {
+                    'messages': messages,
+                    'character_response': character_response
+                }
+            )
+
+        return converted_dataset
+    
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, idx):
+        if self._chat_template is None:
+            raise ValueError("Chat template is not set. Please set the chat template before generating chat.")
+        
+        if self.generation_params is None:
+            raise ValueError("Generation parameters are not set. Please set the generation parameters before generating chat.")
+        
+        chat_input = self._chat_template.render(
+            bos_token=self._tokenizer.special_tokens_map['bos_token'],
+            eos_token=self._tokenizer.special_tokens_map['eos_token'],
+            messages=self.dataset[idx]['messages'],
+            include_beginning_of_conversation=True,
+            add_generation_prompt=True
+        )  
+
+        return chat_input, self.dataset[idx]['character_response']
+    
+    def sample_dataset(self, n: int, session: str):
+        # get indices of the dataset
+        indices = list(range(len(self.dataset)))
+        random.shuffle(indices)
+        indices = indices[:n]
+                
+        return [self[i] for i in indices]
+        
--- dippy/validation_api.py
+++ dippy/validation_api.py
@@ -1,12 +1,20 @@
+from dippy.dataset import PippaDataset
 import torch
+import torch.nn.functional as F
+
 from fastapi import FastAPI, HTTPException
-from transformers import AutoModelForCausalLM, AutoConfig
+from transformers import AutoModelForCausalLM, AutoConfig, AutoTokenizer
 from typing import Any
 
 
 MAX_MODEL_SIZE = 60000 # 60 GB
+SAMPLE_SIZE = 1000
+PROB_TOP_K = 50 # the correct token should be in the top 50 tokens
 device = "cpu" # torch.device("cuda" if torch.cuda.is_available() else "cpu")
 app = FastAPI()
+
+dataset = PippaDataset("data/pippa_deduped.json")
+
 
 def load_model_no_download(model_name: Any):
     """
@@ -34,31 +42,89 @@ def load_model_no_download(model_name: Any):
     else:
         return None, "Could not retrieve model configuration from Hub"
     
-def get_eval_score(model: Any):
+def get_eval_score(model: Any, sampled_data: list[tuple] = None, tokenizer: Any = None):
     """
     Evaluate the model on a dummy task
     """
-    # Blackbox function to evaluate the model
-    # Replace with your actual evaluation code
-    return 0.5
+    # unzip the sampled data
+    contexts, responses = zip(*sampled_data)
+
+    total_prob = 0
+    token_count = 0
+
+    # now we want the probabilities of the model on the sampled in batches of 16
+    batch_size = 16
+    for i in range(0, len(contexts), batch_size):
+        with torch.no_grad():
+            # Tokenize the inputs and labels
+            inputs = tokenizer(contexts[i:i+batch_size], return_tensors="pt", padding=True, truncation=True)
+            labels = tokenizer(responses[i:i+batch_size], return_tensors="pt", padding=True, truncation=True).input_ids
+
+            # Move tensors to the correct device
+            inputs = {k: v.to(device) for k, v in inputs.items()}
+            labels = labels.to(device)
+
+            # Get model predictions (logits)
+            outputs = model(**inputs, labels=labels)
+            logits = outputs.logits
+
+            # Shift the logits to align with the labels
+            shift_logits = logits[..., :-1, :].contiguous()
+
+            # Apply softmax to convert logits to probabilities
+            softmax_probs = F.softmax(shift_logits, dim=-1)
+
+            # 0 out all probabilities that are not in top k. Thus, the correct token should be in the top k, else a score of 0 is given
+            top_k = torch.topk(softmax_probs, k=PROB_TOP_K, dim=-1)
+            softmax_probs = torch.zeros_like(softmax_probs).scatter(dim=-1, index=top_k.indices, src=top_k.values)
+
+            # Gather the probabilities of the correct tokens
+            target_probs = softmax_probs.gather(dim=-1, index=labels[..., 1:].unsqueeze(-1)).squeeze(-1)
+
+            # Sum the probabilities for each batch and accumulate
+            total_prob += target_probs.sum().item()
+            token_count += labels[..., 1:].numel()
+
+    # Calculate the average probability
+    average_prob = total_prob / token_count if token_count > 0 else 0
+
+    return average_prob
+
 
 def evaluate_model(model_name: str):
     model, reason = load_model_no_download(model_name)
     if not model:
         raise HTTPException(status_code=400, detail="Model is not valid: " + reason)
     
+    # Part 1: Check model size and calculate model size score
     # get model size in MB
     model_size = sum(p.numel() for p in model.parameters()) * 4 / 1024 / 1024
 
     if model_size > MAX_MODEL_SIZE:
         raise HTTPException(status_code=400, detail="Model is too large: " + str(model_size) + " MB")
+
+    model_size_score = 1 - (model_size / MAX_MODEL_SIZE)
     
     # Now download the weights
     model = AutoModelForCausalLM.from_pretrained(model_name)
 
-    eval_score = get_eval_score(model)
+    # get the tokenizers
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    dataset.set_chat_template_params("data/vicuna_prompt_template.jinja", tokenizer)
+    sampled_data = dataset.sample_data(SAMPLE_SIZE)
+    # Part 2: Evaluate the model
+    eval_score = get_eval_score(model, sampled_data)
+
+    return {
+        "model_size_score": model_size_score,
+        "qualitative_score": eval_score
+    }
 
 
+@app.get("/validate_model")
+def validate_model(model_name: str):
+    return evaluate_model(model_name)
+ 
 
 
 
