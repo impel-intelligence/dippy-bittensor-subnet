f636ceae0fdc7925ce01d677754a39b525798b6a
Manav Shah <manav@tryimpel.com>
2024-04-19 17:32:32 +0000

    commit before synciing

--- dippy/validation_api.py
+++ dippy/validation_api.py
@@ -295,5 +295,5 @@ def evaluate_model(request: EvaluateModelRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
