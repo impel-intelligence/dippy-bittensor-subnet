971615693124830e336dba5965c8f4685849c717
donald knoller <127629911+donaldknoller@users.noreply.github.com>
2024-06-17 08:50:09 -0400

    Update logging for validation api (#13)
    * Update logging for validation api. Remove validator scoring to be moved to modele queue
    
    * Add result logging

---
+++ utilities/event_logger.py
@@ -0,0 +1,56 @@
+from loguru import logger
+import os,sys
+
+class EventLogger:
+
+
+    def __init__(self, filepath="/tmp/valapi_event_logs/validator_api_{time}.log"):
+        self.logger = logger
+        # Determine the directory part from the filepath
+        log_directory = os.path.dirname(filepath)
+
+        # Check if the directory exists, if not, create it
+        if not os.path.exists(log_directory):
+            try:
+                os.makedirs(log_directory)
+            except PermissionError:
+                raise PermissionError(f"Cannot create log directory at {log_directory}")
+
+        # Check if the directory is writable
+        if not os.access(log_directory, os.W_OK):
+            raise PermissionError(f"The directory {log_directory} is not writable")
+
+        # Configure loguru logger for JSON output and file rotation
+        format = "{time} | {level} | {message}"
+        self.logger.remove()  # Remove default configuration
+        self.logger.add(
+            sys.stderr, format=format, serialize=True  # For JSON output in console
+        )
+        self.logger.add(
+            filepath, rotation="500 MB", format=format, serialize=True  # For JSON output in file with rotation
+        )
+
+    def log(self, level, message, **kwargs):
+        # Include additional fields in the log
+        log_method = getattr(self.logger, level)
+        log_method(message, **kwargs)
+
+    def info(self, message, **kwargs):
+        self.log("info", message, **kwargs)
+
+    def error(self, message, **kwargs):
+        self.log("error", message, **kwargs)
+
+    def debug(self, message, **kwargs):
+        self.log("debug", message, **kwargs)
+
+# Example of using the JsonLogger
+
+def example():
+    try:
+        json_logger = EventLogger()
+        json_logger.info("This is an info message", extra={"user": "admin", "status": "active"})
+        json_logger.error("This is an error essage", extra={"user": "guest", "error_code": 500})
+        json_logger.debug("This is a debug message", extra={"user": "developer", "debug_mode": "on"})
+    except PermissionError as e:
+        print(f"Failed to initialize logger: {e}")
