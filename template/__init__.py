7434e90b1ea42d0292c8873097dc8d3f95c9b8c2
dkoller <dknoller@fastmail.com>
2024-07-12 00:46:16 +0000

    switch to pyproject

--- template/__init__.py
+++ template/__init__.py
@@ -19,7 +19,7 @@
 
 # TODO(developer): Change this value when updating your code base.
 # Define the version of the template module.
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 version_split = __version__.split(".")
 __spec_version__ = (1000 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
 
